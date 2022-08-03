6f4c8695b32d90e0f522e9ba70c53fb1b76dd0a7
a.ershov <a.ershov@vega-gaz.ru>
2022-08-03 09:38:29 +0300

    path

--- main.py
+++ main.py
@@ -7,7 +7,8 @@
     # Use a breakpoint in the code line below to debug your script.
     #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 import easygui
-import io
+#import io
+from pathlib import *
 
 def open_file():
     return easygui.fileopenbox()
@@ -17,11 +18,20 @@ def open_file():
 if __name__ == '__main__':
     #print_hi('Sonata')
     #lnk=open_file()
-    #print(lnk)
-    f = open("G:\Projects\!ConvertPrjToAstraLinux\Design\AT_BWDCAP6QG42EZAS4IIRAMDJDBU.iec_hmi", "wt")
-    print(f)
-    #for line in f:
-    #    f.write(line.replace('WindowFBType', 'GraphicsCompositeFBType'))
+    lnk = "G:\\Projects\\!ConvertPrjToAstraLinux\\Design\\AT_BWDCAP6QG42EZAS4IIRAMDJDBU.iec_hmi"
+    current_dir = PureWindowsPath(lnk)
+    bak = str(current_dir.parents[0]) + "\\tmp.bak"
+    print(bak)
+    f = open(lnk, "r")
+    t = open(bak, "w")
+    #print(f)
+    for line in f:
+        t.write(line.replace('WindowFBType', 'GraphicsCompositeFBType'))
+        #print(line)
     f.close()
+    t.close()
+
+
+
     #easygui.egdemo()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
