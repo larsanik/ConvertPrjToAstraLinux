121eccbe48f78e88e1676bb90be4311f714a9eee
a.ershov <a.ershov@vega-gaz.ru>
2022-08-03 11:16:17 +0300

    dialog

--- main.py
+++ main.py
@@ -16,22 +16,22 @@ def open_file():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #print_hi('Sonata')
-    #lnk=open_file()
-    lnk = "G:\\Projects\\!ConvertPrjToAstraLinux\\Design\\AT_BWDCAP6QG42EZAS4IIRAMDJDBU.iec_hmi"
-    current_dir = PureWindowsPath(lnk)
-    bak = str(current_dir.parents[0]) + "\\tmp.bak"
-    print(bak)
-    f = open(lnk, "r")
-    t = open(bak, "w")
-    #print(f)
-    for line in f:
-        t.write(line.replace('WindowFBType', 'GraphicsCompositeFBType'))
-        #print(line)
-    f.close()
-    t.close()
+    if easygui.ccbox("Выбрать файл IEC HMI и заменить TWindow на TGraphicsComposite?",
+                  "ConvertPrjToAstraLinux",
+                  ("Продолжить", "Выход")):
+        lnk=open_file()
+        if lnk != None:
+            current_dir = PureWindowsPath(lnk)
+            bak = str(current_dir.parents[0]) + "\\tmp.bak"
+            f = open(lnk, "r")
+            t = open(bak, "w")
+        #print(f)
+            for line in f:
+                t.write(line.replace('WindowFBType', 'GraphicsCompositeFBType'))
+                #print(line)
+            f.close()
+            t.close()
 
-
-
-    #easygui.egdemo()
+            easygui.msgbox("Задача завершена", "ConvertProjToAstra", "Выход")
+        #easygui.egdemo()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
